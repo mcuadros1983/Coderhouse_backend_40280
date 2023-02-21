3e7c6407278397ab7ce1c86a5bb06824b039a5ea
Miguel Cuadros <cuadros_miguel@hotmail.com>
2023-02-21 00:37:38 -0300

    desafio_login

---
+++ Desafio09_login_express_sessions/controllers/indexControllers.js
@@ -0,0 +1,30 @@
+const {getMockedItems} = require("../DB/MockApi.js")
+
+// const renderIndex = (req, res) => {
+//   const user = req.session.name
+//   if(req.session.name){
+//     res.render("index", {user});
+//   }else{
+//     res.render("auth/login")
+//   }
+//   console.log("test", req.session.name)
+// }
+
+const renderIndex = (req, res) => {
+  const user = req.session.name
+  if (req.session.name) {
+   res.render("index", { user });
+  } else {
+   res.redirect("auth/login")
+  }
+  console.log("test", req.session.name)
+ }
+
+ 
+const getFakeItems =  (req,res)=>{
+    const products =  getMockedItems(5)
+    res.json(products)
+
+}
+
+module.exports = {renderIndex, getFakeItems};
---
+++ Desafio09_login_express_sessions/server.js
@@ -0,0 +1,94 @@
+require("dotenv").config()
+require("./options/firebase.js")
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const Contenedor = require("./containers/messageContainer.js");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+const { getMockedItems } = require("./DB/MockApi.js")
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const morgan = require("morgan") ;
+const { join } =  require("path");
+const indexRoutes= require("./routes/indexRoutes.js")
+const userRoutes = require("./routes/userRoutes.js")
+const productos = new Contenedor("products");
+const mensajes = new Contenedor("messages");
+const path = require("path")
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+// settings
+app.set("views", join(path.join(__dirname, "views")));
+
+// config view engine
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  layoutsDir: join(app.get("views"), "layouts"),
+  partialsDir: join(app.get("views"), "partials"),
+  extname: ".hbs",
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"))
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_ATLAS,
+      mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      ttl: 100,
+    }),
+    secret: 'secreta',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+// static files
+app.use(express.static(join(__dirname, "public")));
+
+// routes
+app.use(indexRoutes);
+app.use(userRoutes);
+
+//Carga de productos
+io.on("connection", async function (socket) {
+  socket.emit("products", await productos.getAll());
+  socket.on("newProduct", async (data) => {
+    try {
+      await productos.save(data);
+      io.sockets.emit("products", await productos.getAll());
+    } catch (error) {
+      throw new Error(error?.message);
+    }
+  });
+});
+
+//Web Chat
+io.on("connection", async function (socket) {
+  socket.emit("messages", await mensajes.getAllMessagesNormalized());
+  socket.on("newMessage", async (data) => {
+    try {
+      await mensajes.save(data);
+      io.sockets.emit("messages", await mensajes.getAllMessagesNormalized());
+    } catch (error) {
+      throw new Error(error?.message);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+const srv = server.listen(PORT, () =>
+  console.log(
+    `Servidor http con WebSocket escuchando el puerto ${server.address().port}`
+  )
+);
+srv.on("error", (error) => console.log(`Error en servidor ${server}`));
